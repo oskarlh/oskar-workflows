816017f6ac1f51743627da5cc40d2d3f27e178b6
Oskar Larsson Högfeldt <oskar@oskar.pm>
2026-05-07 00:18:32 +0200

    New limit

--- check-package-availability.ts
+++ check-package-availability.ts
@@ -2,9 +2,10 @@ import { setTimeout as asyncSetTimeout } from "node:timers/promises";
 
 const { "dist-tags": { latest }, versions } = await (await fetch("https://registry.npmjs.org/cmath-js")).json();
 
-const { [latest]: { dist: { tarball }} } = versions;
+const { [latest]: { dist: { tarball } } } = versions;
 
-for(let tries = 0; tries < 10; ++tries) {
+const limit = 12 + Math.floor(5 * Math.random());
+for(let tries = 0; tries < limit; ++tries) {
     await asyncSetTimeout(400);
     await (await fetch(tarball, {cache: "no-store"})).blob();
     console.log(`Attempt ${tries}`)
