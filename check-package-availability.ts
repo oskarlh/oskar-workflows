bfeacd1b17e6a1c329df8bfabd5d31abb9e54dbd
Oskar Larsson Högfeldt <oskar@oskar.pm>
2026-05-15 07:36:05 +0200

    Increase DLs

--- check-package-availability.ts
+++ check-package-availability.ts
@@ -1,12 +1,19 @@
 import { setTimeout as asyncSetTimeout } from "node:timers/promises";
 
-const { "dist-tags": { latest }, versions } = await (await fetch("https://registry.npmjs.org/cmath-js")).json();
+const {
+  "dist-tags": { latest },
+  versions,
+} = await (await fetch("https://registry.npmjs.org/cmath-js")).json();
 
-const { [latest]: { dist: { tarball } } } = versions;
+const {
+  [latest]: {
+    dist: { tarball },
+  },
+} = versions;
 
-const limit = 6 + Math.floor(3 * Math.random());
-for(let tries = 0; tries < limit; ++tries) {
-    await asyncSetTimeout(400);
-    await (await fetch(tarball, {cache: "no-store"})).blob();
-    console.log(`Attempt ${tries}`)
+const limit = 8 + Math.floor(3 * Math.random());
+for (let tries = 0; tries < limit; ++tries) {
+  await asyncSetTimeout(400);
+  await (await fetch(tarball, { cache: "no-store" })).blob();
+  console.log(`Attempt ${tries}`);
 }
