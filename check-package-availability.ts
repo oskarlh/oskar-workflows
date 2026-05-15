482be801c3440104ea755f7ec536357a2b2f802d
Oskar Larsson Högfeldt <oskar@oskar.pm>
2026-05-15 07:37:47 +0200

    Formatting

--- check-package-availability.ts
+++ check-package-availability.ts
@@ -1,19 +1,19 @@
 import { setTimeout as asyncSetTimeout } from "node:timers/promises";
 
 const {
-  "dist-tags": { latest },
-  versions,
+	"dist-tags": { latest },
+	versions,
 } = await (await fetch("https://registry.npmjs.org/cmath-js")).json();
 
 const {
-  [latest]: {
-    dist: { tarball },
-  },
+	[latest]: {
+		dist: { tarball },
+	},
 } = versions;
 
 const limit = 8 + Math.floor(3 * Math.random());
 for (let tries = 0; tries < limit; ++tries) {
-  await asyncSetTimeout(400);
-  await (await fetch(tarball, { cache: "no-store" })).blob();
-  console.log(`Attempt ${tries}`);
+	await asyncSetTimeout(400);
+	await (await fetch(tarball, { cache: "no-store" })).blob();
+	console.log(`Attempt ${tries}`);
 }
